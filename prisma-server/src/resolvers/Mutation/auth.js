7a089d9869237ef3dbe828f8bde347dab64e41a7
maabed <veto44@gmail.com>
2018-02-24 18:29:31 +0200

    login screen and update readme

--- prisma-server/src/resolvers/Mutation/auth.js
+++ prisma-server/src/resolvers/Mutation/auth.js
@@ -17,17 +17,30 @@ const auth = {
   async login(parent, { email, password }, ctx, info) {
     const user = await ctx.db.query.user({ where: { email } })
     if (!user) {
-      throw new Error(`No such user found for email: ${email}`)
+      return {
+        error: {
+          field: 'email',
+          msg: 'No such user found for email',
+        }
+      }
     }
 
     const valid = await bcrypt.compare(password, user.password)
     if (!valid) {
       throw new Error('Invalid password')
+      return {
+        error: {
+          field: 'password',
+          msg: 'Invalid password',
+        }
+      }
     }
 
     return {
-      token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
-      user,
+      payload: {
+        token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
+        user,
+      }
     }
   },
 }
