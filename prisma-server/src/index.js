1e6084772590c98e9a15cf292f98fd690b1385e3
maabed <veto44@gmail.com>
2018-02-26 23:05:11 +0200

    fetch images from ststic dir

--- prisma-server/src/index.js
+++ prisma-server/src/index.js
@@ -1,6 +1,7 @@
-const { GraphQLServer } = require('graphql-yoga')
-const { Prisma } = require('prisma-binding')
-const resolvers = require('./resolvers')
+const {GraphQLServer} = require('graphql-yoga');
+const {Prisma} = require('prisma-binding');
+const express = require('express');
+const resolvers = require('./resolvers');
 
 const server = new GraphQLServer({
   typeDefs: 'src/schema.graphql',
@@ -11,9 +12,11 @@ const server = new GraphQLServer({
       typeDefs: 'src/generated/prisma.graphql',
       endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma DB service (value is set in .env)
       secret: process.env.PRISMA_SECRET, // taken from database/prisma.yml (value is set in .env)
-      debug: true, // log all GraphQL queries & mutations
-    }),
-  }),
-})
+      debug: true // log all GraphQL queries & mutations
+    })
+  })
+});
 
-server.start(() => console.log('Server is running on http://localhost:4000'))
+server.express.use('/images', express.static('images'));
+
+server.start(() => console.log('Server is running on http://localhost:4000'));
